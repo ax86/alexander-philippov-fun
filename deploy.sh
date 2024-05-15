ef86aff06f125e7e3d45413c1e903ed876b36fd5
Aleksandr <apple@ax86.com>
2024-05-15 16:32:24 +0300

    Add deploy scripts

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+USER=tim
+HOST=185.26.120.220
+DIR=/var/www/alexanderphilippov.fun/html   # the directory where your web site files should go
+
+hugo && ./remove_originals.sh && rsync -avz --delete public/ ${USER}@${HOST}:${DIR} # this will delete everything on the server that's not in the local public folder 
+
+exit 0
